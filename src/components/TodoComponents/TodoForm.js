bdd311c17b0a1bcad448a09b74f71ef799d79982
Michael Williams <manicdesigner@gmail.com>
2019-06-04 19:54:24 -0400

    update(TodoForm) + name on input

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -11,9 +11,8 @@ class TodoForm extends React.Component {
     render() {
         return(
             <div>
-                <TodoList/>
                 <form>
-                <input type="text" placeholder="...todo" value="stuff" />
+                <input type="text" name="task" placeholder="...todo" value="stuff" />
                 <button type="button" className="add-todo">Add Todo</button>
                 <button type="button" className="clear-completed">Clear Completed</button>
                 </form>
