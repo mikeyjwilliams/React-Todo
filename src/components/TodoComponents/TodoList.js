30d29ae872c180944f07ddb3eb6c425daef61185
Michael Williams <manicdesigner@gmail.com>
2019-06-04 19:36:08 -0400

    js(TodoList.js) + function & Todo component inside TodoList component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,8 +2,13 @@
 // feel free to change this component.js into TodoList.js
 import React from 'react';
 import './Todo.css';
+import Todo from './Todo';
 
 function TodoList() {
-
+    return (
+        <ul>
+            <Todo />
+        </ul>
+    );
 }
 export default TodoList;
