f45223ebada4d706068d895c28a391c603b65f15
Michael Williams <manicdesigner@gmail.com>
2019-06-04 19:34:34 -0400

    js(Todo.js) + function Todo

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Todo.css';
+
+class Todo extends React.Component {
+    constructor() {
+        super();
+        
+    }
+    render() {
+        return (
+            <li>info</li>
+        );
+    }
+}
+export default Todo;
