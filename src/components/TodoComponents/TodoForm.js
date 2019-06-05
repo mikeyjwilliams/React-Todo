31559b815b7d6eea77481425147544690a32d36e
Michael Williams <manicdesigner@gmail.com>
2019-06-04 20:46:26 -0400

    simplify TodoForm.js

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './Todo.css';
-import TodoList from './TodoList';
-import Todo from './Todo';
 
-function TodoForm(props) {
+const TodoForm = props => {
     return(
-        <div>
-            <form>
-            <input type="text" name="task" placeholder="...todo" value={props.item} />
-            <button type="button" className="add-todo">Add Todo</button>
-            <button type="button" className="clear-completed">Clear Completed</button>
-            </form>
-        </div>
+        <form>
+        <input type="text" name="task" placeholder="...todo" value={props.item} />
+        <button type="button" className="add-todo">Add Todo</button>
+        <button type="button" className="clear-completed">Clear Completed</button>
+        </form>
     );
-}
+};
 export default TodoForm;
