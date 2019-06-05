79dac5dd21b5d33a23d0e437fe54280e54b8b2ec
Michael Williams <manicdesigner@gmail.com>
2019-06-04 20:09:54 -0400

    moved to functional component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,22 +3,15 @@ import './Todo.css';
 import TodoList from './TodoList';
 import Todo from './Todo';
 
-class TodoForm extends React.Component {
-    constructor() {
-        super();
-    }
-
-    render() {
-        return(
-            <div>
-                <form>
-                <input type="text" name="task" placeholder="...todo" value="stuff" />
-                <button type="button" className="add-todo">Add Todo</button>
-                <button type="button" className="clear-completed">Clear Completed</button>
-                </form>
-            </div>
-            
-        );
-    }
+function TodoForm(props) {
+    return(
+        <div>
+            <form>
+            <input type="text" name="task" placeholder="...todo" value={props.item} />
+            <button type="button" className="add-todo">Add Todo</button>
+            <button type="button" className="clear-completed">Clear Completed</button>
+            </form>
+        </div>
+    );
 }
 export default TodoForm;
