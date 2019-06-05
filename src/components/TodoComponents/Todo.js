8ab2601252ff943beac68f987daafefdf6ab2684
Michael Williams <manicdesigner@gmail.com>
2019-06-04 21:11:19 -0400

    got rid of extra fluff

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Todo.css';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
+
 
 function Todo() {
     return (
         <div>
-            <TodoList />
-            <TodoForm /> 
+             
         </div>
     );
 }
