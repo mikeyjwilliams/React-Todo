bc59b6978e61bb7c42497d74427bf374856b4c53
Michael Williams <manicdesigner@gmail.com>
2019-06-06 11:36:58 -0400

    testing(Todo.js) + testing toggleItem eventHandler

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Todo.css';
 
+const toggleItem = (props) => {
+    console.log('purchase item change', props.todo.id);
+    // props.toggleItem(props.todo.id);
+}
 
 const Todo = props => {
-    console.log('props in Todo', props);
+    console.log('props in Todo', props.todo);
     return (
-        <div>
+        <div className={`${props.completed ? " completed" : ""}`} onClick={toggleItem}>
             {props.task}
         </div>
     );
